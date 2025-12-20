d1aafeea3a987f91441effc139f7ec8d715eb263
lnoueryo <jounetsism@gmail.com>
2025-12-20 20:18:17 +0900

    feat: grpcを

---
+++ new/grpc_handler.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"context"
+
+	"github.com/pion/webrtc/v4"
+	"github.com/sirupsen/logrus"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/status"
+	"streaming-signaling.jounetsism.biz/proto"
+)
+
+type RoomService struct {
+	signaling.UnimplementedRoomServiceServer
+}
+
+func (s *RoomService) GetRoom(
+	ctx context.Context,
+	req *signaling.GetRoomRequest,
+) (*signaling.GetRoomResponse, error) {
+	var participants []*signaling.Participant
+	room, ok := rooms.getRoom(req.SpaceId)
+	logrus.Infof("GetRoom gRPC called: spaceId=%s, found=%v", req.SpaceId, ok)
+	if !ok {
+		md := metadata.Pairs(
+			"error-code", "room-not-found",
+		)
+		grpc.SetTrailer(ctx, md)
+		return nil, status.Error(codes.NotFound, "roomが存在しません")
+	}
+
+	for _, p := range room.participants {
+		if p.PC.ConnectionState().String() == "connected" {
+			participants = append(participants, &signaling.Participant{
+				Id:    p.ID,
+				Name:  p.Name,
+				Email: p.Email,
+				Image: p.Image,
+			})
+		}
+	}
+
+	return &signaling.GetRoomResponse{
+		Id:           room.ID,
+		Participants: participants,
+	}, nil
+}
+
+func (s *RoomService) RemoveParticipant(
+	ctx context.Context,
+	req *signaling.RemoveParticipantRequest,
+) (*signaling.GetRoomResponse, error) {
+	roomId := req.GetSpaceId()
+	userId := req.GetUserId()
+
+	// ルームが存在しない場合
+	room, ok := rooms.getRoom(roomId)
+	if !ok {
+		return nil, status.Error(codes.NotFound, "no-target-room")
+	}
+
+	// 参加者が存在しない場合
+	participant, ok := room.participants[userId]
+	if !ok {
+		return nil, status.Error(codes.NotFound, "no-target-user")
+	}
+
+	// 切断処理
+	room.listLock.Lock()
+	delete(room.participants, userId)
+	delete(room.wsConnections, participant.WS.Conn)
+
+	// WebRTC のクローズ
+	participant.PC.Close()
+	participant.WS.Send("close", "")
+
+	room.listLock.Unlock()
+
+	// 残った参加者を整形
+	responseParticipants := make([]*signaling.Participant, 0, len(room.participants))
+	for _, p := range room.participants {
+		if p.PC.ConnectionState() == webrtc.PeerConnectionStateConnected {
+			responseParticipants = append(responseParticipants, &signaling.Participant{
+				Id:    p.ID,
+				Name:  p.Name,
+				Email: p.Email,
+				Image: p.Image,
+			})
+		}
+	}
+
+	return &signaling.GetRoomResponse{
+		Id:           room.ID,
+		Participants: responseParticipants,
+	}, nil
+}
